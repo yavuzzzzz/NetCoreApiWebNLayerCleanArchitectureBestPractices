df4909547289b1aa296fa9063922aab7d335e73e
Yavuz Yılmaz <yavuzyilmaz0726@gmail.com>
2025-04-23 15:35:32 +0300

    Program.cs file has reorganized

--- Services/Extensions/ServiceExtensions.cs
+++ Services/Extensions/ServiceExtensions.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using App.Repositories.Products;
-using App.Repositories;
 using App.Services.Categories;
 using App.Services.ExceptionHandler;
 using App.Services.Products;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +14,7 @@ namespace App.Services.Extensions
     {
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
 
